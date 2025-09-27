86c4bf942056329a0ad599adeb8868fceca95323
MnD3v <lolo36626835@gmail.com>
2025-09-27 18:17:17 +0000

    add global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import { GlobalStateProvider } from "./context/GlobalStateContext";
 import Image from "next/image";
 
